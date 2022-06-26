d091dcaf59abadcadda5dff63b954b15971d6960
persiKey <parhomchuk308@gmail.com>
2022-06-26 13:24:36 +0300

    bool vector

--- Source.cpp
+++ Source.cpp
@@ -4,8 +4,9 @@
 #include <map>
 #include <set>
 #include <vector>
+#include <list>
 #include <algorithm>
-
+#include <string>
 
 using std::ofstream;
 using std::ifstream;
@@ -13,9 +14,20 @@ using std::map;
 using std::bitset;
 using std::vector;
 using std::set;
+using std::string;
+using std::list;
 
 using std::int8_t;
 
+typedef vector<bool> b_vector;
+
+
+struct Node
+{
+	string bits;
+	int index;
+
+};
 
 inline bool get_nth_bit(int a, size_t n)
 {
@@ -56,11 +68,22 @@ struct INT_INFO
 		}
 		return out;
 	}
+	bool operator<(const INT_INFO& cmp)
+	{
+		return (cmp.val > this->val) && (cmp.null_shift > this->null_shift);
+	}
+	friend bool operator<(const INT_INFO& A, const INT_INFO& B)
+	{
+		return (A.val > B.val) && (A.null_shift > B.null_shift);
+	}
 };
+
+
 
 
 int main(int argc, char** argv)
 {
+	
 	//short a;
 	//int beg;
 	//int end;
@@ -90,11 +113,58 @@ int main(int argc, char** argv)
 
 	size_t left = 0, right = 0;
 
-	//vector<int8_t> bytes;
+
+	//int8_t buffer;
+	//INT_INFO temp_val; temp_val.val = temp_val.null_shift = 0;
+
+	//vector<INT_INFO> vals;
+	//set<INT_INFO> search_vals;
+
+	//while (!file.eof())
+	//{
+	//	int a;
+
+	//	if (!file.read((char*)&buffer, sizeof(int8_t)))
+	//	{
+	//		break;
+	//	}
+	//	for (int i = sizeof(int8_t) * 8 - 1; i >= 0; --i)
+	//	{
+	//		if (!((i + 1) % 4)) std::cout << ' ';
+	//		std::cout << get_nth_bit(buffer, i);
+	//	}
+	//	do 
+	//	{// Ya kushayu 
+	//		temp_val.val <<= 1;
+	//		temp_val.val += get_nth_bit(buffer, sizeof(int8_t)*8 - left % (sizeof(int8_t)*8) - 1);
+	//		left++;
+	//		++temp_val.null_shift;
+	//		
+	//		if ( search_vals.find(temp_val) == search_vals.end() )
+	//		{
+	//			search_vals.insert(temp_val);
+	//			vals.push_back(temp_val);
+	//			temp_val.val = temp_val.null_shift = 0;
+	//		}
+	//	}while ((left % (sizeof(int8_t) * 8)));
+	//}
+
+	//if(temp_val.null_shift != 0)
+	//	vals.push_back(temp_val);
+	//std::cout << '\n';
+	//for (auto i : vals)
+	//{
+	//	std::cout << i << ' ';
+	//}
+	//std::cout << '\n';
+
 	int8_t buffer;
-	INT_INFO temp_val; temp_val.val = temp_val.null_shift = 0;
-	//set<int> val;
-	vector<INT_INFO> vals;
+	list<std::pair<b_vector, int>> analized;
+	
+	int global_index = 1;
+	int prev_index = 0;
+	map<b_vector, int> vals;
+	std::pair<b_vector, int> temp_val;
 
 	while (!file.eof())
 	{
@@ -102,34 +172,45 @@ int main(int argc, char** argv)
 		{
 			break;
 		}
-		for (int i = sizeof(int8_t) * 8 - 1; i >= 0; --i)
+		//for (int i = sizeof(int8_t) * 8 - 1; i >= 0; --i)
+		//{
+		//	if (!((i + 1) % 4)) std::cout << ' ';
+		//	std::cout << get_nth_bit(buffer, i);
+		//}
+		do
 		{
-			if (!((i + 1) % 4)) std::cout << ' ';
-			std::cout << get_nth_bit(buffer, i);
-		}
-		do 
-		{// Ya kushayu 
-			temp_val.val <<= 1;
-			temp_val.val += get_nth_bit(buffer, sizeof(int8_t)*8 - left % (sizeof(int8_t)*8) - 1);
+			temp_val.first.push_back(get_nth_bit(buffer, sizeof(int8_t) * 8 - left % (sizeof(int8_t) * 8) - 1));
+			
 			left++;
-			++temp_val.null_shift;
-			if ( std::find(vals.begin(), vals.end(), temp_val) == vals.end() )
+			auto a = vals.find(temp_val.first);
+			if (a == vals.end())
 			{
-				vals.push_back(temp_val);
-				temp_val.val = temp_val.null_shift = 0;
+				temp_val.second = global_index;
+				++global_index;
+				vals.insert(temp_val);
+				temp_val.second = prev_index;
+				analized.push_back(temp_val);
+				temp_val.first.clear();
+				prev_index = 0;
 			}
-		}while ((left % (sizeof(int8_t) * 8)));
+			else
+				prev_index = a->second;
+
+		} while ((left % (sizeof(int8_t) * 8)));
 	}
 
-	if(temp_val.null_shift != 0)
-		vals.push_back(temp_val);
-	std::cout << '\n';
-	for (auto i : vals)
+	if (!temp_val.first.empty())
 	{
-		std::cout << i << ' ';
+		temp_val.second = prev_index;
+		analized.push_back(temp_val);
 	}
 	std::cout << '\n';
-
-	
+	for (auto i : analized)
+	{
+		for (auto k : i.first)
+			std::cout << k;
+		std::cout << '-' << i.second << ' ';
+	}
+	std::cout << '\n';
 	return 0;
 }
